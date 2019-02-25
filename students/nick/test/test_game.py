f7d02c631b3161fdb68c04223329b4a7cfb3e8bf
Skeels, Nick <Nick.Skeels@disney.com>
2019-02-25 14:56:35 -0800

    Test cleanup - use setup() method

--- students/nick/test/test_game.py
+++ students/nick/test/test_game.py
@@ -12,19 +12,19 @@ import random
 
 class TestGame(unittest.TestCase):
 
+    def setUp(self):
+        self.deck = Deck()
+        self.deck.shuffle()
+        self.p1 = Player('Bob')
+
     def test_deal_cards(self):
-        test_deck = Deck()
-        test_deck.shuffle()
-        test_player = Player('Bob')
         num_cards = 2
-        game.deal_cards(test_deck, test_player, num_cards)
-        self.assertEqual(num_cards, len(test_player.visible_cards()))
+        game.deal_cards(self.deck, self.p1, num_cards)
+        self.assertEqual(num_cards, len(self.p1.visible_cards()))
 
     def test_dealt_cards_are_different(self):
         num_cards = 10
-        test_deck = Deck()
-        test_deck.shuffle()
-        dealt_cards = set([test_deck.deal() for _ in range(num_cards)])
+        dealt_cards = set([self.deck.deal() for _ in range(num_cards)])
         self.assertEqual(num_cards, len(dealt_cards))
 
     def test_dealt_cards_from_multiple_decks_are_varied(self):
@@ -36,62 +36,49 @@ class TestGame(unittest.TestCase):
             d.shuffle
             test_decks.append(d)
         dealt_cards = [random.choice(test_decks).deal() for _ in range(num_cards)]
+
+        # Counter() will create a list of tuples of the counts of all cards.
         c = Counter(dealt_cards)
+
+        # most_common(n) returns a list of tuples of the n most common elements and their
+        # counts. E.g., [(val, 5), (val2, 3) ...]
         self.assertLessEqual(c.most_common(1)[0][1], num_decks)
 
     def test_basic_score_determination(self):
-        test_deck = Deck()
-        test_deck.shuffle()
-        test_player = Player('Bob')
-        test_player.add_card(Card('5', 'hearts'))
-        test_player.add_card(Card('A', 'diamonds'))
-        self.assertEqual(16, game.get_score(test_player))
+        self.p1.add_card(Card('5', 'hearts'))
+        self.p1.add_card(Card('A', 'diamonds'))
+        self.assertEqual(16, game.get_score(self.p1))
 
     def test_blackjack_score_determination(self):
-        test_deck = Deck()
-        test_deck.shuffle()
-        test_player = Player('Bob')
-        test_player.add_card(Card('10', 'hearts'))
-        test_player.add_card(Card('A', 'diamonds'))
-        self.assertEqual(21, game.get_score(test_player))
+        self.p1.add_card(Card('10', 'hearts'))
+        self.p1.add_card(Card('A', 'diamonds'))
+        self.assertEqual(21, game.get_score(self.p1))
 
     def test_harder_score_determination(self):
-        test_deck = Deck()
-        test_deck.shuffle()
-        test_player = Player('Bob')
-        test_player.add_card(Card('A', 'diamonds'))
-        test_player.add_card(Card('A', 'clubs'))
-        test_player.add_card(Card('10', 'hearts'))
-        test_player.add_card(Card('7', 'diamonds'))
-        self.assertEqual(19, game.get_score(test_player))
+        self.p1.add_card(Card('A', 'diamonds'))
+        self.p1.add_card(Card('A', 'clubs'))
+        self.p1.add_card(Card('10', 'hearts'))
+        self.p1.add_card(Card('7', 'diamonds'))
+        self.assertEqual(19, game.get_score(self.p1))
 
     def test_other_score_determination(self):
-        test_deck = Deck()
-        test_deck.shuffle()
-        test_player = Player('Bob')
-        test_player.add_card(Card('4', 'hearts'))
-        test_player.add_card(Card('7', 'diamonds'))
-        test_player.add_card(Card('A', 'clubs'))
-        self.assertEqual(12, game.get_score(test_player))
+        self.p1.add_card(Card('4', 'hearts'))
+        self.p1.add_card(Card('7', 'diamonds'))
+        self.p1.add_card(Card('A', 'clubs'))
+        self.assertEqual(12, game.get_score(self.p1))
 
     def test_ace_score_over_21(self):
-        test_deck = Deck()
-        test_deck.shuffle()
-        test_player = Player('Bob')
-        test_player.add_card(Card('10', 'hearts'))
-        test_player.add_card(Card('10', 'diamonds'))
-        test_player.add_card(Card('A', 'clubs'))
-        test_player.add_card(Card('A', 'clubs'))
-        self.assertEqual(22, game.get_score(test_player))
+        self.p1.add_card(Card('10', 'hearts'))
+        self.p1.add_card(Card('10', 'diamonds'))
+        self.p1.add_card(Card('A', 'clubs'))
+        self.p1.add_card(Card('A', 'clubs'))
+        self.assertEqual(22, game.get_score(self.p1))
 
     def test_one_ace_11_and_one_1(self):
-        test_deck = Deck()
-        test_deck.shuffle()
-        test_player = Player('Bob')
-        test_player.add_card(Card('9', 'hearts'))
-        test_player.add_card(Card('A', 'clubs'))
-        test_player.add_card(Card('A', 'clubs'))
-        self.assertEqual(21, game.get_score(test_player))
+        self.p1.add_card(Card('9', 'hearts'))
+        self.p1.add_card(Card('A', 'clubs'))
+        self.p1.add_card(Card('A', 'clubs'))
+        self.assertEqual(21, game.get_score(self.p1))
 
 
 if __name__ == '__main__':
