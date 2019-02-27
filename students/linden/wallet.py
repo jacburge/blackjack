0a5d3e7f988c46883586456b33bf882beee1294b
andel078 <linden.anderson@disney.com>
2019-02-27 11:38:20 -0800

    Def

---
+++ students/linden/wallet.py
@@ -0,0 +1,14 @@
+''' This module describes a Bank Class that tracks money and allows debits and credits. '''
+
+class Wallet():
+    ''' Bank Class '''
+
+    def __init__(self, balance: float = 0):
+        self.balance = balance
+
+    def credit(amount: float):
+        # TODO  add txns in log and check for negative, etc.
+        self.balance += amount    
+
+    def debit(amount: float):
+                  # TODO  add txns in log and check for negative, etc.
