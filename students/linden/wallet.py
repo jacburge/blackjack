f0841a67b68c5b7c5dbede59dcb37e9dd61d9492
andel078 <linden.anderson@disney.com>
2019-02-27 12:00:32 -0800

    clean

--- students/linden/wallet.py
+++ students/linden/wallet.py
@@ -1,4 +1,12 @@
-''' This module describes a Bank Class that tracks money and allows debits and credits. '''
+'''
+This module describes a Bank Class that tracks money and allows debits and credits. 
+This assumes a single currency.
+'''
+class NegativeValueError(Exception):
+    """ Exception type for when trying to add a negative, which is a no-no. """
+
+class InsufficientFundsError(Exception):
+    """ Exception type for when trying to remove more money than you have. """
 
 class Wallet():
     ''' Bank Class '''
@@ -6,9 +14,23 @@ class Wallet():
     def __init__(self, balance: float = 0):
         self.balance = balance
 
-    def credit(amount: float):
+    def add_money(self, amount: float) -> float:
+        ''' Add the specified amount to the wallet. Return new balance. '''
         # TODO  add txns in log and check for negative, etc.
-        self.balance += amount    
+        if amount <0:
+            raise NegativeValueError('Amount must be positive.')
 
-    def debit(amount: float):
-                  # TODO  add txns in log and check for negative, etc.
+        self.balance += amount
+        return self.balance
+
+    def remove_money(self, amount: float) -> float:
+        ''' Subtract the amount from the balance and return new balance.None
+        Throws exception.'''
+        # TODO  add txns in log and check for negative, etc.
+        if amount <0:
+            raise NegativeValueError('Amount must be positive.')
+        if amount > self.balance:
+            msg=f'Insufficient funds in wallet. Balance: {self.balance}'
+            raise InsufficientFundsError(msg)
+        self.balance -= amount
+        return self.balance
